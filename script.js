7b85021492edcdfa2bd86d8ba9e0ad7a1c3bdf1b
Bryan <bryan.alessandromc.6061@gmail.com>
2024-08-26 18:30:31 -0400

    * Game board in the console * All the combinations to possibles wins * Print a message to the console indicating who won the game, and a tie message if any combination is not valid.

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 function players() {
-    const player = {
-        player1 () {
-            let mark = 'x';
-            let combo = [1,2,3];
-            return mark;
-        },
-        player2 () {
-            let mark = 'o';
-            let combo = [4,5];
-            return combo;
-        }
-    }
-    return player;
+  const player = {
+      player1 () {
+          let mark = 'x';
+          let combo = [1,2,3];
+          return mark;
+      },
+      player2 () {
+          let mark = 'o';
+          let combo = [4,5];
+          return combo;
+      }
+  }
+  return player;
 }
 
-// function gameBoard() {
-//     const boardDOM = document.getElementById('gameboard');
-//     const game = {
-//         board() {
-//             console.log(boardDOM.childNodes);
-//         }
-//     }
-
-//     return game;
-// }
-
 function game() {
-    let gameboard = [['x'],['o'],['o'],['x'],[''],[''],['x'],[''],['']];
-    //NOTE - el array almacenara dos combinaciones, la del primer jugador y la del segundo, esto quiere decir que dependiendo de la combinacion se dara el ganador
-    let targets = [1,2,3];
-    return {
-        winner() {
-            const cols1 = [gameboard[0], gameboard[1], gameboard[2]];
-            const cols2 = [gameboard[3], gameboard[4],gameboard[5]];
-            const cols3 = [gameboard[6], gameboard[7],gameboard[8]];
-            console.log(cols1[0], cols1[1], cols1[2]);
-            console.log(cols2[0], cols2[1], cols2[2]);
-            console.log(cols3[0], cols3[1], cols3[2]);
-            gameboard.forEach(array => {
-                // console.log(array[0], array[1], array[2]);
-            })
-        },
-        tie() {
-        },
-        loser() {
+  let gameboard = [
+  ['o'],['x'],['o'],
+  ['x'],['x'],['o'],
+  ['o'],['o'],['']
+                  ];
+  let winner = null;
+  return {
+      winner() {
+        console.log(gameboard[0],gameboard[1],gameboard[2]);
+        console.log(gameboard[3], gameboard[4], gameboard[5]);
+        console.log(gameboard[6], gameboard[7], gameboard[8])
+      const winningCombinations = [
+          [gameboard[0], gameboard[1], gameboard[2]], // Row 1
+          [gameboard[3], gameboard[4], gameboard[5]], // Row 2
+          [gameboard[6], gameboard[7], gameboard[8]], // Row 3
+          [gameboard[0], gameboard[3], gameboard[6]], // Column 1
+          [gameboard[1], gameboard[4], gameboard[7]], // Column 2
+          [gameboard[2], gameboard[5], gameboard[8]], // Column 3
+          [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
+          [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
+      ];
+          winningCombinations.forEach(array => {
+            const valueX = array.every((subArray) => subArray.includes('x'));
+            const valueO = array.every((subArray) => subArray.includes('o'))
+            if (valueX) {
+              winner = 'x';
+            } else if (valueO) {
+              winner = 'o';
+            }
+          })
+        if (winner === 'x') {
+          console.log('Player "X" win');
+        } else if (winner === 'gano o') {
+          console.log('Player "O" win');
+        } else {
+          console.log('Tie');
         }
-    }
+      },
+  }
 }
 
 const testing = game().winner;
