7969fc907d161c9ba519e123c92179183f35e436
Bryan <bryan.alessandromc.6061@gmail.com>
2024-08-27 17:32:05 -0400

    * Remove long logicGame (It is in comments for now If I need it later) * Logic when you click the game board on the page

--- script.js
+++ script.js
@@ -1,60 +1,118 @@
-function players() {
-  const player = {
-      player1 () {
-          let mark = 'x';
-          let combo = [1,2,3];
-          return mark;
-      },
-      player2 () {
-          let mark = 'o';
-          let combo = [4,5];
-          return combo;
+// function logicGame() {
+//   let gameboard = [];
+//   let winner = null;
+
+//   return {   
+//       winner(player1,player2) {
+//         gameboard = [
+//           [player2],[player1],[player2],
+//           [player1],[player2],[player2],
+//           [player1],[player1],[player1]
+//         ];
+
+//       console.log(gameboard[0], gameboard[1], gameboard[2]);
+//       console.log(gameboard[3], gameboard[4], gameboard[5]);
+//       console.log(gameboard[6], gameboard[7], gameboard[8]);
+          
+//       const winningCombinations = [
+//           [gameboard[0], gameboard[1], gameboard[2]], // Row 1
+//           [gameboard[3], gameboard[4], gameboard[5]], // Row 2
+//           [gameboard[6], gameboard[7], gameboard[8]], // Row 3
+//           [gameboard[0], gameboard[3], gameboard[6]], // Column 1
+//           [gameboard[1], gameboard[4], gameboard[7]], // Column 2
+//           [gameboard[2], gameboard[5], gameboard[8]], // Column 3
+//           [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
+//           [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
+//       ];
+//           winningCombinations.forEach(array => {
+//             const valueX = array.every((subArray) => subArray.includes('x'));
+//             const valueO = array.every((subArray) => subArray.includes('o'));
+//             if (valueX) {
+//               winner = 'x';
+//             } else if (valueO) {
+//               winner = 'o';
+//             }
+//           });
+//         if (winner === 'x') {
+//           console.log('Player "X" win');
+//         } else if (winner === 'o') {
+//           console.log('Player "O" win');
+//         } else {
+//           console.log('Tie');
+//         }
+//       }
+//   }
+// }
+
+const players = {
+  player1: 'x',
+  player2: 'o',
+}
+
+function addMark() {
+  const boxGame = document.querySelectorAll('.box-game');
+  toggle = false;
+  
+  boxGame.forEach(box => {
+    box.addEventListener('click', function handleToggle() {
+ 
+      if (toggle) {
+        box.textContent = players.player1;
+      } else {
+        box.textContent = players.player2;
       }
-  }
-  return player;
+      toggle = !toggle
+      
+      box.removeEventListener('click', handleToggle)
+    })
+  })
 }
 
-function game() {
-  let gameboard = [
-  ['o'],['x'],['o'],
-  ['x'],['x'],['o'],
-  ['o'],['o'],['']
-                  ];
-  let winner = null;
-  return {
-      winner() {
-        console.log(gameboard[0],gameboard[1],gameboard[2]);
-        console.log(gameboard[3], gameboard[4], gameboard[5]);
-        console.log(gameboard[6], gameboard[7], gameboard[8])
-      const winningCombinations = [
-          [gameboard[0], gameboard[1], gameboard[2]], // Row 1
-          [gameboard[3], gameboard[4], gameboard[5]], // Row 2
-          [gameboard[6], gameboard[7], gameboard[8]], // Row 3
-          [gameboard[0], gameboard[3], gameboard[6]], // Column 1
-          [gameboard[1], gameboard[4], gameboard[7]], // Column 2
-          [gameboard[2], gameboard[5], gameboard[8]], // Column 3
-          [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
-          [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
-      ];
-          winningCombinations.forEach(array => {
-            const valueX = array.every((subArray) => subArray.includes('x'));
-            const valueO = array.every((subArray) => subArray.includes('o'))
-            if (valueX) {
-              winner = 'x';
-            } else if (valueO) {
-              winner = 'o';
-            }
-          })
-        if (winner === 'x') {
-          console.log('Player "X" win');
-        } else if (winner === 'gano o') {
-          console.log('Player "O" win');
-        } else {
-          console.log('Tie');
-        }
-      },
-  }
+addMark();
+
+const gameBoard = {
+  gameboard: [
+    [players.player2],[players.player1],[players.player2],
+    [players.player1],[players.player2],[players.player2],
+    [players.player1],[players.player1],[players.player1]
+  ],  
 }
 
-const testing = game().winner;
-testing();
+const winningCombinations = {
+  combinations:
+  [
+    [gameBoard.gameboard[0], gameBoard.gameboard[1], gameBoard.gameboard[2]], // Row 1
+    [gameBoard.gameboard[3], gameBoard.gameboard[4], gameBoard.gameboard[5]], // Row 2
+    [gameBoard.gameboard[6], gameBoard.gameboard[7], gameBoard.gameboard[8]], // Row 3
+    [gameBoard.gameboard[0], gameBoard.gameboard[3], gameBoard.gameboard[6]], // Column 1
+    [gameBoard.gameboard[1], gameBoard.gameboard[4], gameBoard.gameboard[7]], // Column 2
+    [gameBoard.gameboard[2], gameBoard.gameboard[5], gameBoard.gameboard[8]], // Column 3
+    [gameBoard.gameboard[0], gameBoard.gameboard[4], gameBoard.gameboard[8]], // Diagonal 1
+    [gameBoard.gameboard[2], gameBoard.gameboard[4], gameBoard.gameboard[6]]  // Diagonal 2
+  ]
+}
+
+let winner = null;
+
+function winningValidation() {
+    winningCombinations.combinations.forEach(array => {
+      const valueX = array.every((subArray) => subArray.includes('x'));
+      const valueO = array.every((subArray) => subArray.includes('o'));
+      if (valueX) {
+        winner = 'x';
+      } else if (valueO) {
+        winner = 'o';
+      }
+    });
+      if (winner === 'x') {
+        console.log('Player "X" win');
+      } else if (winner === 'o') {
+        console.log('Player "O" win');
+      } else {
+        console.log('Tie');
+      }
+}
+
+console.log(gameBoard.gameboard[0], gameBoard.gameboard[1], gameBoard.gameboard[2]);
+console.log(gameBoard.gameboard[3], gameBoard.gameboard[4], gameBoard.gameboard[5]);
+console.log(gameBoard.gameboard[6], gameBoard.gameboard[7], gameBoard.gameboard[8]);
