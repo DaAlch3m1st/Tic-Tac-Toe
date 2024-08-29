dc6d38e560bc8d5c27a85174acccb1f2688d3609
Bryan <bryan.alessandromc.6061@gmail.com>
2024-08-29 19:57:41 -0400

    Push the respective content of the players, (no winning validation for now)

--- script.js
+++ script.js
@@ -1,95 +1,22 @@
-// function logicGame() {
-//   let gameboard = [];
-//   let winner = null;
-
-//   return {   
-//       winner(player1,player2) {
-//         gameboard = [
-//           [player2],[player1],[player2],
-//           [player1],[player2],[player2],
-//           [player1],[player1],[player1]
-//         ];
-
-//       console.log(gameboard[0], gameboard[1], gameboard[2]);
-//       console.log(gameboard[3], gameboard[4], gameboard[5]);
-//       console.log(gameboard[6], gameboard[7], gameboard[8]);
-          
-//       const winningCombinations = [
-//           [gameboard[0], gameboard[1], gameboard[2]], // Row 1
-//           [gameboard[3], gameboard[4], gameboard[5]], // Row 2
-//           [gameboard[6], gameboard[7], gameboard[8]], // Row 3
-//           [gameboard[0], gameboard[3], gameboard[6]], // Column 1
-//           [gameboard[1], gameboard[4], gameboard[7]], // Column 2
-//           [gameboard[2], gameboard[5], gameboard[8]], // Column 3
-//           [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
-//           [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
-//       ];
-//           winningCombinations.forEach(array => {
-//             const valueX = array.every((subArray) => subArray.includes('x'));
-//             const valueO = array.every((subArray) => subArray.includes('o'));
-//             if (valueX) {
-//               winner = 'x';
-//             } else if (valueO) {
-//               winner = 'o';
-//             }
-//           });
-//         if (winner === 'x') {
-//           console.log('Player "X" win');
-//         } else if (winner === 'o') {
-//           console.log('Player "O" win');
-//         } else {
-//           console.log('Tie');
-//         }
-//       }
-//   }
-// }
-
 const players = {
   player1: 'x',
-  player2: 'o',
+  player2: 'o'
 }
 
-function addMark() {
-  const boxGame = document.querySelectorAll('.box-game');
-  toggle = false;
-  
-  boxGame.forEach(box => {
-    box.addEventListener('click', function handleToggle() {
- 
-      if (toggle) {
-        box.textContent = players.player1;
-      } else {
-        box.textContent = players.player2;
-      }
-      toggle = !toggle
-      
-      box.removeEventListener('click', handleToggle)
-    })
-  })
-}
-
-addMark();
-
-const gameBoard = {
-  gameboard: [
-    [players.player2],[players.player1],[players.player2],
-    [players.player1],[players.player2],[players.player2],
-    [players.player1],[players.player1],[players.player1]
-  ],  
-}
+let gameboard = Array(9).fill('');
 
 const winningCombinations = {
   combinations:
   [
-    [gameBoard.gameboard[0], gameBoard.gameboard[1], gameBoard.gameboard[2]], // Row 1
-    [gameBoard.gameboard[3], gameBoard.gameboard[4], gameBoard.gameboard[5]], // Row 2
-    [gameBoard.gameboard[6], gameBoard.gameboard[7], gameBoard.gameboard[8]], // Row 3
-    [gameBoard.gameboard[0], gameBoard.gameboard[3], gameBoard.gameboard[6]], // Column 1
-    [gameBoard.gameboard[1], gameBoard.gameboard[4], gameBoard.gameboard[7]], // Column 2
-    [gameBoard.gameboard[2], gameBoard.gameboard[5], gameBoard.gameboard[8]], // Column 3
-    [gameBoard.gameboard[0], gameBoard.gameboard[4], gameBoard.gameboard[8]], // Diagonal 1
-    [gameBoard.gameboard[2], gameBoard.gameboard[4], gameBoard.gameboard[6]]  // Diagonal 2
-  ]
+    [gameboard[0], gameboard[1], gameboard[2]], // Row 1
+    [gameboard[3], gameboard[4], gameboard[5]], // Row 2
+    [gameboard[6], gameboard[7], gameboard[8]], // Row 3
+    [gameboard[0], gameboard[3], gameboard[6]], // Column 1
+    [gameboard[1], gameboard[4], gameboard[7]], // Column 2
+    [gameboard[2], gameboard[5], gameboard[8]], // Column 3
+    [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
+    [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
+  ],
 }
 
 let winner = null;
@@ -111,8 +38,51 @@ function winningValidation() {
       } else {
         console.log('Tie');
       }
+      return;
+}
+winningValidation();
+
+function conseguirIndex() {
+  const g = document.getElementById('gameboard');
+  let toggleWin = false;
+  for (let i = 0; i < g.children.length; i++) {
+    (function(index) {
+      g.children[i].onclick = function() {
+        console.log(index);
+        if (!toggleWin) {
+          gameboard[index] = 'x';
+          console.log(gameboard);
+        } else {
+          gameboard[index] = 'o';
+          console.log(gameboard);
+        }
+        toggleWin = !toggleWin;
+      }
+    })(i);
+  }
+
+  handleBoxes();
+
+  function handleBoxes() {
+    const boxes = document.querySelectorAll('.box-game');
+    let toggle = false;
+
+    boxes.forEach(box => {
+      box.addEventListener('click', function handleToggle() {
+        if (toggle) {
+          box.textContent = 'x';
+        } else {
+          box.textContent = 'o';
+        }
+        toggle = !toggle;
+        console.log(toggle)
+        console.log(gameboard[0], gameboard[1], gameboard[2]);
+        console.log(gameboard[3], gameboard[4], gameboard[5]);
+        console.log(gameboard[6], gameboard[7], gameboard[8]);
+        box.removeEventListener('click', handleToggle);
+      });
+    });
+  }
 }
 
-console.log(gameBoard.gameboard[0], gameBoard.gameboard[1], gameBoard.gameboard[2]);
-console.log(gameBoard.gameboard[3], gameBoard.gameboard[4], gameBoard.gameboard[5]);
-console.log(gameBoard.gameboard[6], gameBoard.gameboard[7], gameBoard.gameboard[8]);
+conseguirIndex();
