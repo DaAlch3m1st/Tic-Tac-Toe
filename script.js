d8abe5b519472da865951e0606492789814f80e3
Bryan <bryan.alessandromc.6061@gmail.com>
2024-09-02 11:59:16 -0400

    * Winning validation to the content of the gameboard in the page, and the message on the console * Function to reset the gameboard (no working right now) *TODO - Solve the problem of the function reset

--- script.js
+++ script.js
@@ -3,86 +3,77 @@ const players = {
   player2: 'o'
 }
 
-let gameboard = Array(9).fill('');
-
-const winningCombinations = {
-  combinations:
-  [
-    [gameboard[0], gameboard[1], gameboard[2]], // Row 1
-    [gameboard[3], gameboard[4], gameboard[5]], // Row 2
-    [gameboard[6], gameboard[7], gameboard[8]], // Row 3
-    [gameboard[0], gameboard[3], gameboard[6]], // Column 1
-    [gameboard[1], gameboard[4], gameboard[7]], // Column 2
-    [gameboard[2], gameboard[5], gameboard[8]], // Column 3
-    [gameboard[0], gameboard[4], gameboard[8]], // Diagonal 1
-    [gameboard[2], gameboard[4], gameboard[6]]  // Diagonal 2
-  ],
+const gameboard = {
+  array: 
+    Array(9).fill('')
 }
 
-let winner = null;
+const boxes = document.querySelectorAll('.box-game');
+const getCombos = () => {
+  return {
+      combos: [
+          [boxes[0].textContent, boxes[1].textContent, boxes[2].textContent], // Row 1
+          [boxes[3].textContent, boxes[4].textContent, boxes[5].textContent], // Row 2
+          [boxes[6].textContent, boxes[7].textContent, boxes[8].textContent], // Row 3
+          [boxes[0].textContent, boxes[3].textContent, boxes[6].textContent], // Column 1
+          [boxes[1].textContent, boxes[4].textContent, boxes[7].textContent], // Column 2
+          [boxes[2].textContent, boxes[5].textContent, boxes[8].textContent], // Column 3
+          [boxes[0].textContent, boxes[4].textContent, boxes[8].textContent], // Diagonal 1
+          [boxes[2].textContent, boxes[4].textContent, boxes[6].textContent]  // Diagonal 2
+      ]
+  };
+}
 
-function winningValidation() {
-    winningCombinations.combinations.forEach(array => {
-      const valueX = array.every((subArray) => subArray.includes('x'));
-      const valueO = array.every((subArray) => subArray.includes('o'));
-      if (valueX) {
-        winner = 'x';
-      } else if (valueO) {
-        winner = 'o';
-      }
-    });
-      if (winner === 'x') {
-        console.log('Player "X" win');
-      } else if (winner === 'o') {
-        console.log('Player "O" win');
+function winningCombinations() {
+  let winner = null;
+  const currentCombos = getCombos();
+  currentCombos.combos.forEach(array => {
+    const valueX = array.every((subArray) => subArray.includes(players.player1));
+    const valueO = array.every((subArray) => subArray.includes(players.player2));
+    if (valueX) {
+      winner = players.player1;
+    } else if (valueO) {
+      winner = players.player2;
+    }
+  });
+  if (winner === players.player1) {
+    console.log('Player "X" win');
+  } else if (winner === players.player2) {
+    console.log('Player "O" win');
+  } else {
+    console.log('Tie');
+  }
+}
+
+function emptyBoard() {
+  const resetBtn = document.getElementById('reset');
+  boxes.forEach(arr => {
+    resetBtn.addEventListener('click', function() {
+      arr.textContent='';
+    })
+  })
+}
+
+emptyBoard();
+
+function dom() {
+  const boxes = document.querySelectorAll('.box-game')
+  let toggle = false;
+
+  boxes.forEach(box => {
+    box.addEventListener('click', function handleToggle() {
+      const currentCombos = getCombos();
+      console.log(currentCombos.combos);
+      if (!toggle) {
+        box.textContent = players.player1;
       } else {
-        console.log('Tie');
+        box.textContent = players.player2;
       }
-      return;
-}
-winningValidation();
-
-function conseguirIndex() {
-  const g = document.getElementById('gameboard');
-  let toggleWin = false;
-  for (let i = 0; i < g.children.length; i++) {
-    (function(index) {
-      g.children[i].onclick = function() {
-        console.log(index);
-        if (!toggleWin) {
-          gameboard[index] = 'x';
-          console.log(gameboard);
-        } else {
-          gameboard[index] = 'o';
-          console.log(gameboard);
-        }
-        toggleWin = !toggleWin;
-      }
-    })(i);
-  }
-
-  handleBoxes();
-
-  function handleBoxes() {
-    const boxes = document.querySelectorAll('.box-game');
-    let toggle = false;
-
-    boxes.forEach(box => {
-      box.addEventListener('click', function handleToggle() {
-        if (toggle) {
-          box.textContent = 'x';
-        } else {
-          box.textContent = 'o';
-        }
-        toggle = !toggle;
-        console.log(toggle)
-        console.log(gameboard[0], gameboard[1], gameboard[2]);
-        console.log(gameboard[3], gameboard[4], gameboard[5]);
-        console.log(gameboard[6], gameboard[7], gameboard[8]);
-        box.removeEventListener('click', handleToggle);
-      });
+      toggle = !toggle;
+      winningCombinations();
+      box.removeEventListener('click', handleToggle);
     });
-  }
+  })
 }
 
-conseguirIndex();
+dom();
